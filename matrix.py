9017f24898db22e70a924c325c94e743d84eca2b
Kaio <128155244+Kaio0809@users.noreply.github.com>
2023-06-13 18:56:33 +0000

    quase

--- matrix.py
+++ matrix.py
@@ -2,15 +2,19 @@
 
 
 def soma(x: list[list[float]], y: list[list[float]]) -> list[list[float]]:
-    """Soma duas matrizes"""
-    if(len(x) != len(y) or len(x[0]) != len(y[0])):
-        return None
-    result = []
-    for i in range(len(x)):   
-        result.append([])
-        for j in range(len(x[0])):
-            result[i].append(x[i[j]] + y[i[j]])
-    return result
+    soma_matrizes = []
+    if x == [] and y == []:
+        return []
+    else:
+        if len(x) == len(y) and len(x[0]) == len(y[0]):
+            for i in range(len(x)):
+                soma_matrizes.append([])
+                for j in range(len(x[0])):
+                    somas = x[i][j] + y[i][j]
+                    soma_matrizes[i].append(somas)
+            return soma_matrizes
+        else:
+            return None
 
 
 
@@ -35,13 +39,10 @@ def multiplicação(x: list[list[float]], y: list[list[float]]) -> list[list[flo
         return None
         
     else:
-        rowsx = len(x)
-        cols_x = len(x[0])
-        cols_y = len(y[0])
-        C = [[0 for _ in range(cols_y)] for _ in range(cols_x)]
-        for i in range(rowsx):
-            for j in range(cols_y):
-                for k in range(cols_x):
+        C = [[0 for _ in range(len(y[0]))] for _ in range(len(x[0]))]
+        for i in range(len(x)):
+            for j in range(len(y[0])):
+                for k in range(len(x[0])):
                     C[i][j] += x[i][k] * y[k][j]
         return C
 
@@ -53,22 +54,41 @@ def norma(x: list[list[float]]) -> float:
     norm = 0
     for i in x:
         for j in i:
-            q = j**2
+            q = j*j
             l.append(q)
-        for number in l:
-            norm += number
+    for number in l:
+        norm += number
     norma_final = norm**0.5
     return norma_final
 
 
 def é_simétrica(x: list[list[float]]) -> bool:
     """Verifica se uma matriz é simétrica"""
-    # TODO: implementar
+    if x == []:
+        return True
+    if len(x) == len(x[0]) and transposta(x) == x:
+        return True
+    else:
+        return False
+    # : implementar
     # uma matriz é simétrica se ela é quadrada e se ela é igual a sua transposta
     # a transposta de uma matriz é a matriz que tem as linhas da matriz original como colunas e as colunas da matriz original como linhas
 
 
 def transposta(x: list[list[float]]) -> list[list[float]]:
     """Calcula a transposta de uma matriz"""
-    # TODO: implementar
+    transpst = []
+    if x == []:
+        return []
+    for i in range(len(x[0])):
+        transpst.append([])
+        for j in range(len(x)):
+            transpst[i].append(x[j][i])
+
+    return transpst
+ 
+
+
+
+    #: implementar
     # a transposta de uma matriz [[1, 2, 4], [2, 3, 4]] é [[1, 2], [2, 3], [4, 4]]
